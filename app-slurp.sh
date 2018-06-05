2ea53c3bb1d94c915f8d27919b6ac932e465aa7d
hes <hes@csail.mit.edu>
2018-06-05 14:09:47 -0400

    Adding scripts to push and pull from all git repos

---
+++ app-slurp.sh
@@ -0,0 +1,14 @@
+cd ~/josh-dist/
+git pull
+
+cd ~/Research-Projects/new-awdrat/
+git pull
+
+cd ~/Research-Projects/attack-planning/
+git pull
+
+cd ~/Research-Projects/control-system/
+git pull
+
+cd ~/Research-Projects/natural-software/
+git pull
---
+++ app-update-script.sh
@@ -0,0 +1,24 @@
+cd ~/josh-dist/
+git commit -a -m "Updating all apps"
+git push origin
+git push mit
+
+cd ~/Research-Projects/new-awdrat/
+git commit -a -m "Updating all apps"
+git push origin
+git push mit
+
+cd ~/Research-Projects/attack-planning/
+git commit -a -m "Updating all apps"
+git push origin
+git push mit
+
+cd ~/Research-Projects/control-system/
+git commit -a -m "Updating all apps"
+git push origin
+git push mit
+
+cd ~/Research-Projects/natural-software/
+git commit -a -m "Updating all apps"
+git push origin
+git push mit
