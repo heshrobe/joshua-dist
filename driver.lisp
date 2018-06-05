f81fed6a522010e5851ee36946fd34080cb1eb5d
hes <hes@csail.mit.edu>
2018-06-05 14:40:34 -0400

    Better version of driver.lisp

--- driver.lisp
+++ driver.lisp
@@ -22,35 +22,43 @@
   ;;; note that this one doesn't need the mcl :external-format because it's for allegro
   ;;; only
   #+allegro
-  (load (make-pathname :directory `(:absolute ,@directory)
-		       :device (pathname-device loading-file)
-		       :name "system-class" :type "lisp"))
-  (load (make-pathname :directory `(:absolute ,@directory "xml-parser")
-		       :device (pathname-device loading-file)
-		       :name "xml-parser-defsystem" :type "lisp"))
-  (load (make-pathname :directory `(:absolute ,@directory "sample-xml-rpc-server")
-		       :device (pathname-device loading-file)
-		       :name "defsystem" :type "lisp"))
-  (load (make-pathname :directory `(:absolute ,@directory "clim-fixes") 
-		       :device (pathname-device loading-file)
-		       :name "clim-fixes-defsystem" :type "lisp")
-        #+mcl :external-format #+mcl :unix)
-  (load (make-pathname :directory `(:absolute ,@directory "clim-env" "portable-lisp-environment") 
-		       :device (pathname-device loading-file)
-		       :name "load-env" :type "lisp")
-        #+mcl :external-format #+mcl :unix)
-  (load (make-pathname :directory `(:absolute ,@directory "joshua" "code")
-		       :device (pathname-device loading-file)
-		       :name "joshua-defsystem" :type "lisp")
-        #+mcl :external-format #+mcl :unix)
-  (load (make-pathname :directory `(:absolute ,@directory "joshua" "developer")
-		       :device (pathname-device loading-file)
-		       :name "jd-defsystem" :type "lisp")
-        #+mcl :external-format #+mcl :unix)
-  (load (make-pathname :directory `(:absolute ,@directory "ideal")
-		       :device (pathname-device loading-file)
-		       :name "load-ideal" :type "lisp"))
-  )
+   (load (make-pathname :directory `(:absolute ,@directory)
+			:device (pathname-device loading-file)
+			:name "system-class" :type "lisp"))
+   (load (make-pathname :directory `(:absolute ,@directory "xml-parser")
+			:device (pathname-device loading-file)
+			:name "xml-parser-defsystem" :type "lisp"))
+   (load (make-pathname :directory `(:absolute ,@directory "sample-xml-rpc-server")
+			:device (pathname-device loading-file)
+			:name "defsystem" :type "lisp"))
+   (load (make-pathname :directory `(:absolute ,@directory "clim-fixes") 
+			:device (pathname-device loading-file)
+			:name "clim-fixes-defsystem" :type "lisp")
+	 #+mcl :external-format #+mcl :unix)
+   (load (make-pathname :directory `(:absolute ,@directory "clim-env" "portable-lisp-environment") 
+			:device (pathname-device loading-file)
+			:name "load-env" :type "lisp")
+	 #+mcl :external-format #+mcl :unix)
+   (load (make-pathname :directory `(:absolute ,@directory "joshua" "code")
+			:device (pathname-device loading-file)
+			:name "joshua-defsystem" :type "lisp")
+	 #+mcl :external-format #+mcl :unix)
+   (load (make-pathname :directory `(:absolute ,@directory "joshua" "developer")
+			:device (pathname-device loading-file)
+			:name "jd-defsystem" :type "lisp")
+	 #+mcl :external-format #+mcl :unix)
+   (load (make-pathname :directory `(:absolute ,@directory "ideal")
+			:device (pathname-device loading-file)
+			:name "load-ideal" :type "lisp"))
+   ;; load the defsystems for the all the current apps
+   (load "~/Research-Projects/natural-software/code/defsystem.lisp")
+  
+   (load "~/Research-Projects/new-awdrat/code/defsystem.lisp")
+  
+   (load "~/Research-Projects/control-system/defsystem.lisp")
+  
+   (load "~/Research-Projects/attack-planning/defsystem.lisp")
+   )
 
   
 #+Allegro
@@ -58,7 +66,7 @@
   (setq excl:*additional-logical-pathname-name-chars* '(#\_ #\*)))
 
 
-(defun build-it (&key compile recompile xml-server)
+(defun build-it (&key compile recompile xml-server apps)
   ;; clim-fixes
   (when compile
     (compile-system 'clim-fixes :recompile recompile))
@@ -86,4 +94,17 @@
     (when compile
       (compile-system 'sample-xml-rpc-server :recompile recompile))
     (load-system 'sample-xml-rpc-server))
+  (when apps
+    (when compile
+      (compile-system 'natsoft :recompile recompile))
+    (load-system 'natsoft)
+    (when compile
+      (compile-system 'awdrat :recompile recompile))
+    (load-system 'awdrat)
+    (when compile
+      (compile-system 'controls :recompile recompile))
+    (load-system 'controls)
+    (when compile
+      (compile-system 'aplan :recompile recompile))
+    (load-system 'aplan))    
   )
