f280c46192330188d4a52a42bc633bcb1e949083
hes <hes@csail.mit.edu>
2019-06-12 14:47:55 -0400

    Updating all apps

--- app-update-script.sh
+++ app-update-script.sh
@@ -1,23 +1,28 @@
 cd ~/josh-dist/
+echo "Doing Joshua Distribution"
 git commit -a -m "Updating all apps"
 git push origin
 git push mit
 
+echo "Doing AWDRAT"
 cd ~/Research-Projects/awdrat/
 git commit -a -m "Updating all apps"
 git push origin
 git push mit
 
+echo "Doing Attack Planner"
 cd ~/Research-Projects/attack-planning/
 git commit -a -m "Updating all apps"
 git push origin
 git push mit
 
+echo "Doing Control System Demo"
 cd ~/Research-Projects/control-system/
 git commit -a -m "Updating all apps"
 git push origin
 git push mit
 
+echo "Doing NatSoft"
 cd ~/Research-Projects/natural-software/
 git commit -a -m "Updating all apps"
 git push origin
