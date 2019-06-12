f1a3fc1d87c5b80a6f53d31b1e9cacfd948183d2
hes <hes@csail.mit.edu>
2019-06-12 14:53:27 -0400

    Updating all apps

--- app-update-script.sh
+++ app-update-script.sh
@@ -1,39 +1,39 @@
 cd ~/josh-dist/
-echo "Doing Joshua Distribution\n"
+echo "\nDoing Joshua Distribution\n"
 git commit -a -m "Updating all apps"
-echo "Pushing to Github.com"
+echo "\nPushing to Github.com"
 git push origin
-echo "Pushing to MIT Github"
+echo "\nPushing to MIT Github"
 git push mit
 
-echo "Doing AWDRAT\n"
+echo "\nDoing AWDRAT\n"
 cd ~/Research-Projects/awdrat/
 git commit -a -m "Updating all apps"
-echo "Pushing to Github.com"
+echo "\nPushing to Github.com"
 git push origin
-echo "Pushing to MIT Github"
+echo "\nPushing to MIT Github"
 git push mit
 
-echo "Doing Attack Planner\n"
+echo "\nDoing Attack Planner\n"
 cd ~/Research-Projects/attack-planning/
 git commit -a -m "Updating all apps"
-echo "Pushing to Github.com"
+echo "\nPushing to Github.com"
 git push origin
-echo "Pushing to MIT Githu"b
+echo "\nPushing to MIT Githu"b
 git push mit
 
-echo "Doing Control System Demo\n"
+echo "\nDoing Control System Demo\n"
 cd ~/Research-Projects/control-system/
 git commit -a -m "Updating all apps"
-echo "Pushing to Github.com"
+echo "\nshing to Github.com"
 git push origin
-echo "Pushing to MIT Github"
+echo "\nPushing to MIT Github"
 git push mit
 
-echo "Doing NatSoft\n"
+echo "\nDoing NatSoft\n"
 cd ~/Research-Projects/natural-software/
 git commit -a -m "Updating all apps"
-echo "Pushing to Github.com"
+echo "\nPushing to Github.com"
 git push origin
-echo "Pushing to MIT Github"
+echo "\nPushing to MIT Github"
 git push mit
