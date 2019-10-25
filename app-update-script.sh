9b26ac1b2bad60b55d92febe753bac020549f710
hes <hes@csail.mit.edu>
2019-10-25 18:35:47 -0400

    Updating all apps

--- app-update-script.sh
+++ app-update-script.sh
@@ -18,22 +18,27 @@ echo "\nPushing to MIT Github"
 git push mit
 fi
 
+if test -e "~/Research-Projects/attack-planning"; then
 echo "\nDoing Attack Planner\n"
 cd ~/Research-Projects/attack-planning/
 git commit -a -m "Updating all apps"
 echo "\nPushing to Github.com"
 git push origin
-echo "\nPushing to MIT Githu"b
+echo "\nPushing to MIT Githu"
 git push mit
+fi
 
+if test -e "~/Research-Projects/control-system"; then
 echo "\nDoing Control System Demo\n"
 cd ~/Research-Projects/control-system/
 git commit -a -m "Updating all apps"
-echo "\nshing to Github.com"
+echo "\npushing to Github.com"
 git push origin
 echo "\nPushing to MIT Github"
 git push mit
+fi
 
+if test -e "~/Research-Projects/natural-software"; then
 echo "\nDoing NatSoft\n"
 cd ~/Research-Projects/natural-software/
 git commit -a -m "Updating all apps"
@@ -41,3 +46,4 @@ echo "\nPushing to Github.com"
 git push origin
 echo "\nPushing to MIT Github"
 git push mit
+fi
