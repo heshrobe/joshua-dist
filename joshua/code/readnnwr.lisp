0a7fde5d405dc3f7009b3772e225dc652898ebff
hes <hes@csail.mit.edu>
2021-06-24 13:43:17 -0400

    Just a few things

--- joshua/code/readnnwr.lisp
+++ joshua/code/readnnwr.lisp
@@ -949,3 +949,36 @@ Joshua readtable and the JI, JU, and Joshua packages. "
 (set-pprint-dispatch '(cons (member *backquote-comma-dot-flag*)) #'joshua-comma-dot-printer)
 (set-pprint-dispatch '(cons (member *backquote-comma-atsign-flag*)) #'joshua-comma-atsign-printer)
  )
+
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defmacro with-keywords-removed ((new-list list keywords-to-remove) &body body)
+    `(let ((,new-list (remove-keywords ,list ,keywords-to-remove)))
+       ,@body))
+
+  (defun remove-keywords (list keywords)
+    (macrolet ((remove-keywords-1 (name-var predicate-form)
+                 `(let ((head nil)
+                        (tail nil))
+                    (do ()
+                        ((null list))
+                      (let ((,name-var (pop list))
+                            (value (pop list)))
+                        (unless ,predicate-form
+                          (setq tail (setq head (list ,name-var value)))
+                          (return))))
+                    (do ()
+                        ((null list) head)
+                      (let ((,name-var (pop list))
+                            (value (pop list)))
+                        (unless ,predicate-form
+                          (setq tail (setf (cddr tail) (list ,name-var value)))))))))
+      (cond ((null list) nil)
+            ((null keywords) list)
+            ;; Special case: use EQ instead of MEMBER when only one keyword is supplied.
+            ((null (cdr keywords))
+             (let ((keyword (car keywords)))
+               (remove-keywords-1 name (eq name keyword))))
+            (t
+             (remove-keywords-1 name (member name keywords))))))
+  )
