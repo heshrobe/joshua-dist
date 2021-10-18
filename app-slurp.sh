d6980f381f24b46ed7f4b772ebeaebadad7c0903
hes <hes@csail.mit.edu>
2021-10-18 14:25:07 -0400

    Updating all code

--- app-slurp.sh
+++ app-slurp.sh
@@ -2,14 +2,14 @@
 
 echo "Doing Joshua"
 cd ~/joshua-dist/
-git pull
+git pull mit mastert
 
 echo "Doing AWDRAT"
 FILE=~/Research-Projects/awdrat
 if test -e "$FILE";
    then echo "Pulling AWDRAT"
 	cd ~/Research-Projects/awdrat/
-	git pull
+	git pull mit master
 else echo "AWDRAT Directory doesn't exist.  Skipping"
      fi
 
@@ -18,7 +18,7 @@ FILE=~/Research-Projects/attack-planning
 if test -e "$FILE";
    then echo "Pulling ATTACK-PLANNER"
 	cd ~/Research-Projects/attack-planning/
-	git pull
+	git pull mit master
 else echo "Attack Planner Directory doesn't exist.  Skipping"
      fi
 
@@ -27,7 +27,7 @@ FILE=~/Research-Projects/control-system
 if test -e "$FILE";
    then echo "Pulling Control System"
 	cd ~/Research-Projects/control-system/
-	git pull
+	git pull mit master
 else "Control System Directory doesn't exist.  Skipping"
      fi
 
@@ -37,7 +37,7 @@ FILE=~/Research-Projects/natural-software
 if test -e "$FILE";
    then echo "Pulling Natural Software"
 	cd ~/Research-Projects/natural-software/
-	git pull
+	git pull mit master
 else "Natural Software Directory doesn't exist.  Skipping"
 fi
 
@@ -46,7 +46,7 @@ FILE=~/Research-Projects/start-interface
 if test -e "$FILE";
    then echo "Pulling Start Interface"
 	cd ~/Research-Projects/start-interface/
-	git pull
+	git pull mit master
 else "Start Interface Directory doesn't exist.  Skipping"
      fi
 
@@ -55,7 +55,7 @@ FILE=~/Research-Projects/recipes
 if test -e "$FILE";
    then echo "Pulling Recipes"
 	cd ~/Research-Projects/recipes/
-	git pull
+	git pull mit master
 else "Recipes Directory doesn't exist.  Skipping"
      fi
 
@@ -64,15 +64,15 @@ FILE=~/Research-Projects/ASIST/guide
 if test -e "$FILE";
    then echo "Pulling Guide"
 	cd ~/Research-Projects/ASIST/guide/
-	git pull
+	git pull mit master
 else "Guide Directory doesn't exist.  Skipping"
      fi
 
-echo "Doing Guide"
+echo "Doing McClim Additions"
 FILE=~/quicklisp/local-projects/mcclim-additions
 if test -e "$FILE";
    then echo "Pulling Guide"
 	cd ~/quicklisp/local-projects/mcclim-additions/
-	git pull
+	git pull mit master
 else "MccClim Additions doesn't exist.  Skipping"
      fi
